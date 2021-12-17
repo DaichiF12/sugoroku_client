3d00ad2668badd11951fe1b81a627f7401f978a2
DaichiF12 <96008738+DaichiF12@users.noreply.github.com>
2021-12-17 12:18:57 +0900

    Lobby.javaのコード記述
    変更できてますか？

--- netcom/Lobby.java
+++ netcom/Lobby.java
@@ -1,4 +1,72 @@
+import java.util.ArrayList;
 class Lobby
 {
+  private ArrayList<User> users;
+  private String lobbyID;
+  private boolean isRandom;
+  private ArrayList<boolean> readyList;
+  private String lobbyPassword;
 
+  public Lobby(String id, String pass, boolean patternFlag)
+  {
+    this.users = new ArrayList<>();
+    this.lobbyID = id;
+    this.isRandom = patternFlag;
+    this.readyList = new ArrayList<>();
+    this.lobbyPassword = pass;
+  }
+
+  public String getLobbyID()
+  {
+    return this.lobbyID;
+  }
+
+  public int getUserNum()
+  {
+    return this.users.size();
+  }
+
+  public int getUserNum(String userid)
+  {
+    int upos;
+    String idtmp;
+
+    for(upos = 0; upos < this.getUserNum(); upos++)
+    {
+    idtmp = this.users.get(upos).getName();
+
+      if(idtmp.equals(userid))
+       {
+         break;
+       }
+    }
+    return upos;
+  }
+
+  public ArrayList<User> getUserList()
+  {
+    return this.users;
+  }
+
+  public void setReady(String userid)
+  {
+    int pos = this.getUserNum(userid);
+    this.readyList.set(pos,true);
+  }
+
+  public void addUser(User user)
+  {
+    this.users.add(user);
+  }
+
+  public void deleteUser(String userid)
+  {
+    int pos = this.getUserNum(userid);
+    this.users.remove(pos);
+  }
+
+  public String getPassword()
+  {
+    return this.lobbyPassword;
+  }
 }
